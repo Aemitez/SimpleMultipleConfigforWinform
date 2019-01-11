4bd54e2bc8b5493fe1ed689b56e63fbd34cce49e
Aemitez <watcharapong_aem@hotmail.com>
2019-01-11 14:30:03 +0700

    Add files via upload

---
+++ SimpleMultipleConfigforWinform/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+using System.Windows.Forms;
+using SimpleMultipleConfigforWinform.Properties;
+
+namespace SimpleMultipleConfigforWinform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = Settings.Default.Setting;
+            label4.Text = ConfigurationManager.AppSettings["appSettings"];
+            if (!string.IsNullOrEmpty(ConfigurationManager.AppSettings["Productionvalue"]))
+            {
+                label5.Visible = true;
+                label6.Visible = true;
+                label6.Text = ConfigurationManager.AppSettings["Productionvalue"];
+            }
+                
+        }
+    }
+}
